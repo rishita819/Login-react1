4df1b54c737e4d678a3dc0381bf914a9795484ef
Rishita Chaturvedi <rishitachaturvediofficial@gmail.com>
2024-06-27 23:38:22 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <div className="app-container"></div>
+    </>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return <h1 className="login-header">Login</h1>;
+}
+
+export default Header;
